3f84cda128a1f3e0aec69bcb6d7cb1fdb0123948
Clarity-89 <homes89@ukr.net>
2023-04-13 09:41:11 +0300

    Add v5 prompt version

---
+++ FormPromptReactRouterV5.js
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { Prompt } from "react-router-dom";
+
+const stepLinks = ["/contact", "/education", "/about", "/confirm"];
+
+export const FormPrompt = ({ hasUnsavedChanges }) => {
+  useEffect(() => {
+    const onBeforeUnload = (e) => {
+      if (hasUnsavedChanges) {
+        e.preventDefault();
+        e.returnValue = "";
+      }
+    };
+    window.addEventListener("beforeunload", onBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", onBeforeUnload);
+    };
+  }, [hasUnsavedChanges]);
+
+  const onLocationChange = (location) => {
+    if (stepLinks.includes(location.pathname)) {
+      return true;
+    }
+    return "You have unsaved changes, are you sure you want to leave?";
+  };
+
+  return <Prompt when={hasUnsavedChanges} message={onLocationChange} />;
+};
