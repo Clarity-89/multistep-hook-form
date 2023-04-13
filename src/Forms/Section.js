c703dea37e65ed79d0ff8c2d2d7f291bc174e837
Clarity-89 <homes89@ukr.net>
2023-04-13 14:05:13 +0300

    Update section
    (cherry picked from commit 78abf0a65b0d8a5c8776b570d3d599237b61ea41)

--- src/Forms/Section.js
+++ src/Forms/Section.js
@@ -1,15 +1,13 @@
-import { useNavigate } from "react-router-dom";
-import { Button } from "./Button";
+import { Link } from "react-router-dom";
 
 export const Section = ({ title, children, url }) => {
-  const navigate = useNavigate();
   return (
     <div className="section mb-4">
       <div className="title-row mb-4">
         <h4>{title}</h4>
-        <Button type="button" variant="secondary" onClick={() => navigate(url)}>
+        <Link className={`btn btn-secondary`} to={url}>
           Edit
-        </Button>
+        </Link>
       </div>
       <div className="content">{children}</div>
     </div>
